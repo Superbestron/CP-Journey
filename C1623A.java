81990fa066d703e532f795d44f343ddae103e4b4
Ng Tze Henn <65549665+Superbestron@users.noreply.github.com>
2022-10-28 22:17:02 +0800

    Robot Cleaner

---
+++ C1623A.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class C1623A {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int rb = sc.nextInt();
+            int cb = sc.nextInt();
+            int rd = sc.nextInt();
+            int cd = sc.nextInt();
+            int dr = 1;
+            int dc = 1;
+            int time = 0;
+
+            while (rb != rd && cb != cd) {
+                time++;
+                if (rb == n && dr == 1 || rb == 1 && dr == -1) {
+                    dr = -dr;
+                }
+                if (cb == m && dc == 1 || cb == 1 && dc == -1) {
+                    dc = -dc;
+                }
+
+                rb += dr;
+                cb += dc;
+                // System.out.println(rb);
+                // System.out.println(cb);
+
+            }
+            System.out.println(time);
+        }
+    }
+}
