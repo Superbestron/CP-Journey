1ba2296278244c6947d0da358e77f94ae54f7e57
Ng Tze Henn <65549665+Superbestron@users.noreply.github.com>
2022-10-28 22:21:15 +0800

    Mirror in the String

--- C1616B.java
+++ C1616B.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+
 public class C1616B {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+
+        while (tc-- > 0) {
+            int n = sc.nextInt();
+            sc.nextLine();
+            String str = sc.nextLine();
+            char c = '{';
+            int i = 0;
+            if (str.length() >= 2) {
+                if (str.charAt(0) == str.charAt(1)) {
+                    System.out.println(str.substring(0, 2));
+                    continue;
+                }
+            }
+            while (i < n) {
+                char curr = str.charAt(i);
+                // if curr letter is greater than prev, cfm dw take
+                if (curr <= c) {
+                    c = curr;
+                } else {
+                    break;
+                }
+                i++;
+            }
+            if (i == 0) i = n;
+
+            StringBuilder ans = new StringBuilder(str.substring(0, i));
+            StringBuilder rev = new StringBuilder(ans).reverse();
+            ans.append(rev);
+            System.out.println(ans);
+        }
+    }
 }
