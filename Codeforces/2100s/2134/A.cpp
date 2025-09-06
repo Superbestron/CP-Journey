bcdae0547dbb6f0caf47988b31743cd8ce483e68
Ng Tze Henn <tzehenn@gmail.com>
2025-09-06 22:31:54 +0800

    Sep 2025

---
+++ Codeforces/2100s/2134/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+int t, n;
+
+inline void solve() {
+    cin >> n;
+    vi a(n);
+    for (int &i : a) cin >> i;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
